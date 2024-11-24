838b99cab7c61a31f8760c602030c3405b36aff5
nocturnalelk07 <finnp@outlook.com>
2024-11-24 14:25:16 +0000

    home screen nav bar added

--- app/src/main/java/com/example/mobileappscw/HomeScreen.kt
+++ app/src/main/java/com/example/mobileappscw/HomeScreen.kt
@@ -1,8 +1,14 @@
 package com.example.mobileappscw
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.snackbar.Snackbar
 
 class HomeScreen : AppCompatActivity() {
 
@@ -10,5 +16,30 @@ class HomeScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_screen)
 
+        val toolbar = findViewById<Toolbar>(R.id.homeToolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_layout, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val myView = findViewById<View>(R.id.homeToolbar)
+        when (item.itemId) {
+            //this is just an example snack bar
+            R.id.profilePicture -> {
+                val snackbar =
+                    Snackbar.make(myView, "profile was pressed",0)
+                snackbar.show()
+                return true
+            }
+            R.id.settingsAction -> {
+                val settingsIntent = Intent(this, SettingsScreen::class.java)
+                startActivity(settingsIntent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
